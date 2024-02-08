7b2427a1c63e3d932a90b13ce6a8e5367464a705
em-henken <118228404+em-henken@users.noreply.github.com>
2024-02-07 19:08:39 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([37.7, -122.4], 10);
+var map = L.map('map').setView([40.7826, -73.9656], 10);
 
   // load a tile layer
 L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -10,12 +10,23 @@ L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
   // load GeoJSON from an external file
-$.getJSON("https://raw.githubusercontent.com/em-henken/gis4091_project1_leaflet/main/Public_Schools_Points_2011-2012A.json", function (data) {
-  console.log(data)
-
+$.getJSON({url:"https://raw.githubusercontent.com/em-henken/gis4091_project1_leaflet/main/new-york-city-boroughs.geojson", dataType: "json"},function (data) {
   L.geoJson(data, {
-    pointToLayer: function (feature, latlng) {
-      return L.marker(latlng, { color: 'blue' });
+    style: function (feature, latlng) {
+      return {color:'blue'};
     }
   }).addTo(map);
 });
+
+$.getJSON("https://raw.githubusercontent.com/em-henken/gis4091_project1_leaflet/main/districts1.geojson",function (data) {
+  L.geoJson(data, {
+    style: function (feature, latlng) {
+      return {color:'black'};
+    }
+  }).addTo(map);
+});
+
+var popup = L.popup()
+    .setLatLng([40.7826, -74.056])
+    .setContent("New York City Bouroughs and Schools Districts")
+    .openOn(map);
